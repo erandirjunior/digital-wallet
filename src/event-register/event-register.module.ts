5c16335ab7b483f91360cc91a1ac2c17e852eafb
Erandir <aefs12junior@gmail.com>
2023-02-13 16:46:49 -0300

    Removed unnecessary code

--- src/event-register/event-register.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import {EventRegisterController} from './event-register.controller';
-
-@Module({
-    controllers: [EventRegisterController]
-})
-export class EventRegisterModule {}
