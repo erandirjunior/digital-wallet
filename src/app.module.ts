358fe310977e6c8da6c235fa9315b072e79486e5
Erandir <aefs12junior@gmail.com>
2023-02-15 13:45:38 -0300

    Added modules and project settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,15 @@ import { User } from './user/user.entity';
 import { Transaction } from './operation/transaction.entity';
 import { OperationModule } from './operation/operation.module';
 import { CommandBus } from '@nestjs/cqrs';
+import { user1676472190481 } from '../migrations/1676472190481-user';
+import { transaction1676472206949 } from '../migrations/1676472206949-transaction';
+import { config } from './orm-config';
+
+console.log(config);
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
+    TypeOrmModule.forRoot(config/*{
       type: 'mysql',
       host: process.env.DB_HOST,
       port: 3306,
@@ -16,7 +21,11 @@ import { CommandBus } from '@nestjs/cqrs';
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
       entities: [User, Transaction],
-    }),
+      migrations: [user1676472190481, transaction1676472206949],
+      cli: {
+        migrationsDir: 'src/migrations'
+      }
+    }*/),
     UserModule,
     OperationModule
   ]
--- src/main.ts
+++ src/main.ts
@@ -7,20 +7,19 @@ async function bootstrap() {
   const port = process.env.APP_PORT
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
-      new ValidationPipe({
-        whitelist: true,
-        transform: true,
-      }),
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
   );
-    const config = new DocumentBuilder()
-        .setTitle('Cats example')
-        .setDescription('The cats API description')
-        .setVersion('1.0')
-        .addTag('cats')
-        .build();
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api', app, document);
+  const config = new DocumentBuilder()
+    .setTitle('Digital Wallet')
+    .setDescription('The digital wallet API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
-    await app.listen(port);
+  await app.listen(port);
 }
 bootstrap();
